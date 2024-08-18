40788567081bb46511e7f003959b935f83d73b84
Alexander Ognerubov <a.ulianovskiy@yandex.ru>
2024-08-18 08:49:35 +0300

    init commit

---
+++ MobileUpGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileUpGallery
+//
+//  Created by Alexander Ognerubov on 18.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileUpGallery/MobileUpGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileUpGalleryApp.swift
+//  MobileUpGallery
+//
+//  Created by Alexander Ognerubov on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileUpGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
